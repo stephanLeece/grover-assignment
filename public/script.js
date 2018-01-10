24cae67003cd0760ff9481edd04961e6cd57cd1f
Stephan Leece <stephan.leece@outlook.com>
2018-01-10 18:58:21 +0100

    call and response added, with basic value parsing

--- public/script.js
+++ public/script.js
@@ -4,7 +4,15 @@
       if(e.which == 13) {
         if($(textarea).val().match(/[^\s]/))
           console.log('enter');
-          $('main').append(`<div class='message'>heya</div>`)
-          $(textarea).val('');
+          $('main').append(`<div class='message'>test</div>`)
+          console.log($(textarea).val());
+          if($(textarea).val() == 'hello') {
+            setTimeout(()=> {$('main').append(`<div class='message'>hey! can i help?</div>`);$(textarea).val('');}, 1000)
+          }
+          else {
+            setTimeout(()=> {$('main').append(`<div class='message'>Sorry, i'm not sure...</div>`);$(textarea).val('');}, 1000)
+          }
+
+
       }
   });
