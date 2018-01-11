6816eae46b8379d31e530c63220f71628400b28e
Stephan Leece <stephan.leece@outlook.com>
2018-01-11 11:21:02 +0100

    connected up to dialog flow. can now make small talk with bot

--- public/script.js
+++ public/script.js
@@ -4,15 +4,17 @@
       if(e.which == 13) {
         if($(textarea).val().match(/[^\s]/))
           console.log('enter');
-          $('main').append(`<div class='message'>test</div>`)
-          console.log($(textarea).val());
-          if($(textarea).val() == 'hello') {
-            setTimeout(()=> {$('main').append(`<div class='message'>hey! can i help?</div>`);$(textarea).val('');}, 1000)
-          }
-          else {
-            setTimeout(()=> {$('main').append(`<div class='message'>Sorry, i'm not sure...</div>`);$(textarea).val('');}, 1000)
-          }
+          $('main').append(`<div class='message'>${$(textarea).val()}</div>`)
+          $.post({
+            url: "/chat",
+            data: {userMessage: $(textarea).val()},
+            success: function (data) {
+              $(textarea).val('')
+              $('main').append(`<div class='message'>${data}</div>`)
+              console.log(data);
+            },
 
+          });
 
       }
   });
--- server.js
+++ server.js
@@ -1,14 +1,45 @@
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
 const express = require('express');
+const bodyParser = require('body-parser');
 const server = express();
+const apiai = require('apiai');
+const botChat = apiai('4c6b4f2cd5dc4e87b871026a524c5b08');
 
+
+
+
+
+
+
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 server.use(express.static(__dirname + `/public`));
+
+
+
 
 server.get('/', function(req, res) {
     (res.sendFile('index.html'));
 });
 
+
+server.post("/chat", function(request, response) {
+  console.log(request.body);
+  var request = botChat.textRequest(request.body.userMessage, {
+      sessionId: '1'
+  });
+
+  request.on('response', function(reply) {
+      response.send(reply.result.fulfillment.speech);
+  });
+
+  request.on('error', function(error) {
+      console.log(error);
+  });
+
+  request.end();
+
+
+  });
 
 
 
