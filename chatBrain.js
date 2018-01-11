74ceb4ae53e228920a4971201d9604d1dadbdb2b
Stephan Leece <stephan.leece@outlook.com>
2018-01-11 12:40:35 +0100

    setting up chat conditions

--- chatBrain.js
+++ chatBrain.js
@@ -3,7 +3,9 @@ const data = require('./data.json');
 module.exports.sayHello = (query) => {
   console.log('hello from the chatBrain');
   let reply;
-
+  if (query.contains("iphone")) {
+    reply = 'iphone'
+  }
 
 
 
