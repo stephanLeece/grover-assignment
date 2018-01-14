2761b0ebf763dd65ec20b56cc1a4d447ae08eede
stephan <stephan.leece@outlook.com>
2018-01-14 21:29:26 +0100

    small fixes - added img alt, scroll to bottom on user msg

--- public/script.js
+++ public/script.js
@@ -4,30 +4,33 @@
 // setTimeout used to stop bot replying instantaneously.
 
 const welcomeMessages = ['Hi! What are you looking for today?', 'Hey there. What are you thinking of renting?',
-  'Hello! What are you interested in?']
+    'Hello! What are you interested in?'];
 
 $('#botWelcomeMsg').html(welcomeMessages[Math.floor(Math.random() * welcomeMessages.length)]);
 
 $(document).keypress(function(e) {
-  if (e.which == 13) {
-    if ($(textarea).val().match(/[^\s]/)) {
-      $('.messageWindow').append(`<div class='message userMessage'>${$(textarea).val()}</div>`)
-      $.post({
-        url: "/chat",
-        data: {
-          userMessage: $(textarea).val()
-        },
-        success: function(data) {
-          $(textarea).val('')
-          setTimeout(() => {
-            $('.messageWindow').append(`<div class='message botMessage'>${data}</div>`);
-            $(".messageWindow").stop().animate({
-              scrollTop: $(".messageWindow")[0].scrollHeight
+    if (e.which == 13) {
+        if ($('#textArea').val().match(/[^\s]/)) {
+            $('#messageWindow').append(`<div class='message userMessage'>${$('#textArea').val()}</div>`);
+            $("#messageWindow").stop().animate({
+                scrollTop: $("#messageWindow")[0].scrollHeight
             }, 500);
-          }, 1000)
+            $.post({
+                url: "/chat",
+                data: {
+                    userMessage: $('#textArea').val()
+                },
+                success: function(data) {
+                    $('#textArea').val('');
+                    setTimeout(() => {
+                        $('#messageWindow').append(`<div class='message botMessage'>${data}</div>`);
+                        $("#messageWindow").stop().animate({
+                            scrollTop: $("#messageWindow")[0].scrollHeight
+                        }, 500);
+                    }, 1000);
 
+                }
+            });
         }
-      });
     }
-  }
 });
